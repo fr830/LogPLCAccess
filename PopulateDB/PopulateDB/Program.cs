9bc59bf3893765d373e970acac7b9de1bfbb0781
jimfede <jimfede@gmail.com>
2017-10-31 11:12:25 -0300

    primera parte hecha, bajar archivo de URL a disco
    se hace un workarround del certificado ssl del plc

--- PopulateDB/PopulateDB/Program.cs
+++ PopulateDB/PopulateDB/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,16 @@ namespace PopulateDB
     {
         static void Main(string[] args)
         {
+            if (System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
+            {
+                {
+                    WebClient Client = new WebClient();
+                    string url = "https://10.26.32.200/DataLog.html?FileName=temperaturacamaras.csv";
+                    string destino = @"C:\LogPLC\temperaturacamaras.csv";
+                    ServicePointManager.ServerCertificateValidationCallback = (senderX, certificate, chain, sslPolicyErrors) => { return true; };
+                    Client.DownloadFile(url, destino);
+                }
+            }
         }
     }
 }
